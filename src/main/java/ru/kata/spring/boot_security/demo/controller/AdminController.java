2b642001e4804f5c42193b0a039570e3bd831a3d
ilnur <ranzolaa@gmail.com>
2025-01-27 17:39:08 +0500

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,65 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Controller()
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final RoleRepository roleRepository;
+
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(RoleRepository roleRepository, UserService userService) {
+        this.roleRepository = roleRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+        model.addAttribute("users", userService.getAll());
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String newUser (Model model){
+        model.addAttribute("allRoles", roleRepository.findAll());
+        model.addAttribute("user", new User());
+        return "user-create";
+    }
+
+    @PostMapping("/new")
+    public String create(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam Long id) {
+        userService.delete(id);
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("/update")
+    public String updateForm(@RequestParam Long id, Model model) {
+        User user = userService.getById(id);
+        model.addAttribute("allRoles", roleRepository.findAll());
+        model.addAttribute("user", user);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute("user") User user) {
+        userService.update(user);
+        System.out.println("AAAAAAAAAAAAAAAA");
+        return "redirect:/admin/";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+
+public interface UserService extends UserDetailsService {
+    void saveUser(User user);
+
+    List<User> getAll();
+
+    User getById(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+
+    User findByUsername(String username);
+}
